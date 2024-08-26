2136d125db40f785da2b73cd15ba068b16be0582
JemGodspeed <cemkadir_92@hotmail.com>
2024-08-26 19:00:38 -0400

    added another paragraph

---
+++ Basic_HTML_and_JavaScript_1/Basic_Javascript1.js
@@ -0,0 +1,7 @@
+
+alert("hello world i am an alert box!!!!");
+   function my_first_function() {
+            var string = "Kiss me, I'm Irish!";
+            var result = string.fontcolor("green");
+            document.getElementById("irish").innerHTML = result;
+        }
